ce198318adc0cbcc16d384f71e1c793295915891
chosww <chosww@gmail.com>
2019-11-25 14:43:23 +0000

    Deploying to gh-pages from master 222fb348c36f66f3bcd3f01e2ee99d4c1f82ebe2

---
+++ precache-manifest.307e2b14d01b4cef21a456e209ffb3a8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e96a039c28b1723fd597e4fe80810308",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ca5fe9be7a2523370ccb",
+    "url": "/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "4d4a0ed2a51906bc4657",
+    "url": "/static/js/2.c630ccb0.chunk.js"
+  },
+  {
+    "revision": "ca5fe9be7a2523370ccb",
+    "url": "/static/js/main.eb8e817f.chunk.js"
+  },
+  {
+    "revision": "034b3d9abd3c1fe13545",
+    "url": "/static/js/runtime-main.56965584.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/static/media/logo.25bf045c.svg"
+  }
+]);
